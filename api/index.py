2b228ecdbeae61c644e0d8b27e8a81b0f97e639b
Shyam Sundar <shyamsundarv00073@gmail.com>
2025-01-29 18:51:23 +0530

    temp: remove all route catcher

--- api/index.py
+++ api/index.py
@@ -38,19 +38,6 @@ def get_logs():
 def home():
     return jsonify({"message": "Server is running"}), 200
 
-# Modified catch-all route to return a 404 response
-@app.route('/<path:path>', methods=['GET', 'POST'])
-def catch_all(path):
-    return jsonify({"error": "Not found"}), 404
-
-# Error handlers
-@app.errorhandler(404)
-def not_found_error(error):
-    return jsonify({"error": "Not found"}), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return jsonify({"error": "Internal server error"}), 500
 
 # For Vercel deployment
 application = app
